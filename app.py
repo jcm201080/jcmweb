f0983da6cfa8b642d44d285c8cc3befb4e52013f
Jesús Castaño <jcm201080@gmail.com>
2026-03-11 09:43:08 +0100

    modificar base de datos

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
     "DATABASE_URL",
-    "sqlite:///" + os.path.join(BASE_DIR, "instance", "visitas.db")
+    "sqlite:////var/www/jcmweb_flask/instance/visitas.db"
 )
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
